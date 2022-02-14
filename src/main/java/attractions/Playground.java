7264f2ea01d01e46c2fd7fb806266a1b0e9fdfbe
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-02-14 22:08:52 +0000

    made adjustments with ISecurity

--- src/main/java/attractions/Playground.java
+++ src/main/java/attractions/Playground.java
@@ -1,8 +1,19 @@
 package attractions;
 
-public class Playground extends Attraction {
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class Playground extends Attraction implements ISecurity {
 
     public Playground(String name, int rating) {
         super(name, rating);
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        if(visitor.getAge() >=12 && visitor.getHeight() >=145){
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/stalls/TobaccoStall.java
+++ src/main/java/stalls/TobaccoStall.java
@@ -1,8 +1,19 @@
 package stalls;
 
-public class TobaccoStall extends Stall {
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, ParkingSpot parkingSpot) {
         super(name, ownerName, parkingSpot);
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        if(visitor.getAge() >=18){
+            return true;
+        }
+        return false;
     }
 }
